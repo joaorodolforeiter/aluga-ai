79be357f2c269376f38e8daf87d35dc7d04c6942
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2024-02-12 21:15:04 -0300

    fix pagination

--- src/main/java/com/alugai/alugaai/repository/ProductRepository.java
+++ src/main/java/com/alugai/alugaai/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.alugai.alugaai.repository;
 
 import com.alugai.alugaai.domain.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,10 +9,10 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findByNameContaining(String name, Pageable pageable);
+    Page<Product> findByNameContaining(String name, Pageable pageable);
 
     List<Product> findByCategoryName(String name);
 
-    List<Product> findByCategoryName(String name, Pageable pageable);
+    Page<Product> findByCategoryName(String name, Pageable pageable);
 
 }
